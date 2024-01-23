bf7c1d59943712a840ffbf007fe97de8b58cdded
Alan TL <teamlemons.cr@gmail.com>
2024-01-23 15:12:46 -0300

    se adquieren datos correctamente mediante la interrupcion del rtc, y mediante la interrupcion del pulsador se habilita el servidor

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,22 @@
 
 // Variables globales y configuraciones
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 0, 111);
+IPAddress ip(192, 168, 0, 101);
 EthernetServer server(80);
 RTC_DS3231 rtc;
 
 #define CLOCK_INTERRUPT_PIN 18
+#define SERVER_INTERRUPT_PIN 19
 #define CHIP_SELECT_PIN 40
 
 volatile bool alarmTriggered = false;
+volatile bool pulsadorServidor = false;
 
 void onAlarm() {
   alarmTriggered = true;
+}
+void onPulsador() {
+  pulsadorServidor = true;
 }
 
 void setup() {
@@ -38,23 +43,27 @@ void setup() {
   }
 
   pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP);
+  pinMode(SERVER_INTERRUPT_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), onAlarm, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SERVER_INTERRUPT_PIN), onPulsador, FALLING);
+  
   rtc.clearAlarm(1);
-
   rtc.writeSqwPinMode(DS3231_OFF);
   rtc.disableAlarm(2);
 
-  DateTime alarmTime(2023, 11, 28, 10, 40, 0);
+  DateTime alarmTime = rtc.now();
+  alarmTime = alarmTime + TimeSpan(0, 0, 1, 0);
+
 
   if (!rtc.setAlarm1(alarmTime, DS3231_A1_Second)) {
     Serial.print("Error al configurar alarma");
   }
-/*
+
   if (!SD.begin(CHIP_SELECT_PIN)) {
     Serial.println("Error al iniciar la tarjeta SD");
     while (1);
   }
-*/
+
   Serial.println("Servidor en:");
   Serial.println(Ethernet.localIP());
 }
@@ -142,9 +151,16 @@ void sendDefaultMessage(EthernetClient client) {
   client.println("<li><a href=\"/temperature\">Temperatura Page</a></li>");
   client.println("<li><a href=\"/update-time\">Actualizar Hora</a></li>");
   client.println("</ul>");
+
+  // Agregar el formulario con el botón para salir
+  client.println("<form action=\"/exit\" method=\"post\">");
+  client.println("<input type=\"submit\" value=\"Salir\">");
+  client.println("</form>");
+
   client.println("</body>");
   client.println("</html>");
 }
+
 
 void sendUpdatePage(EthernetClient client) {
   client.println("HTTP/1.1 200 OK");
@@ -193,7 +209,7 @@ void acquireAndSaveData() {
   Serial.print(" Temp: ");
   Serial.print(temperature);
   Serial.println(" C");
-/*
+
   File dataFile = SD.open("data.txt", FILE_WRITE);
   if (dataFile) {
     dataFile.print(rtc.now().timestamp());
@@ -204,7 +220,7 @@ void acquireAndSaveData() {
   } else {
       Serial.println("Error al abrir el archivo para escritura");
   }
-*/
+
 }
 
 void enableServer() {
@@ -218,7 +234,11 @@ void enableServer() {
         client.flush();
         Serial.println(request);
 
-        if (request.indexOf("GET /data") != -1) {
+        if (request.indexOf("POST /exit") != -1) {
+          // Modificar la variable para salir del bucle
+          pulsadorServidor = false;
+          break;
+        } else if (request.indexOf("GET /data") != -1) {
           sendData(client);
         } else if (request.indexOf("GET /time") != -1) {
           sendTimeMessage(client);
@@ -239,19 +259,21 @@ void enableServer() {
   }
 }
 
+
 void loop() {
 
-  detachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN));
-
+  detachInterrupt(0);
   if (alarmTriggered) {
-    
     acquireAndSaveData();
     rtc.clearAlarm(1);
     alarmTriggered = false;
   }
-  //if (/* condición para habilitar el servidor */) {
-    enableServer(); // Habilitar el servidor y manejar las solicitudes
-  //}
 
-  //LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  while (pulsadorServidor) {
+    enableServer(); // Habilitar el servidor
+  }
+    // Dormir hasta que ocurra una interrupción
+  Serial.println("A dormir...");
+  delay(3000);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
